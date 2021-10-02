a45e8b5ccb42837bfe504d634541c60495551abf
Henry Kam <h3nrykam@gmail.com>
2021-10-02 15:46:35 -0700

    create method outlines

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+
+
+
+//default will be cupertino
+
+//fetch current weather from api
+
+// fetch forecast from api
+
+// get input from search bar, event listener for search bar
+
+// render search buttons, from local storage
+
+// render todays weather
+
+//render forecast
+
