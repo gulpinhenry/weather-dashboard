00c0025fc3479c0b931da21581b0636c721bb9c4
Henry Kam <h3nrykam@gmail.com>
2021-10-02 16:21:20 -0700

    add search history buttons and its handlers

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,14 @@
+var savedHistoryEl = $("#saved-history");
 
-
+//this is temporary
+localStorage.setItem(0,"Cupertino");
+localStorage.setItem(1,"New York City");
+localStorage.setItem(2,"Boston");
+localStorage.setItem(3,"Plano");
+localStorage.setItem(4,"Princeton");
+localStorage.setItem(5,"Fresno");
+localStorage.setItem(6,"Berkeley");
+localStorage.setItem(7,"Omaha");
 
 //default will be cupertino
 
@@ -9,9 +18,27 @@
 
 // get input from search bar, event listener for search bar
 
+console.log("hi");
+
 // render search buttons, from local storage
+var renderSearchButtons = function(){
+    //retrieve from local storage
+    for(let i = 0; i<localStorage.length; i++)
+    {
+        let curEl = savedHistoryEl.children().eq(i);
+        curEl.attr("data-city", localStorage.getItem(i));
+    }
+
+};
+//get input from search buttons, event listener
+function savedHistoryHandler(event){
+    let location = event.target.getAttribute("data-city");
+};
+savedHistoryEl.on("click", savedHistoryHandler);
 
 // render todays weather
 
 //render forecast
 
+//render starting page
+renderSearchButtons();
